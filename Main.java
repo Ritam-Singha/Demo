842918ab4406d75ebf44b78edf018b3835b0987e
Ritam Singha <ritamsingha80@gmail.com>
2024-12-05 00:41:14 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+
+class Main
+{
+    public static void main(String a[]){
+        System.out.println("Hello World");
+        String alpha = "beta";
+        System.out.println(alpha);
+        int x =4;
+        if (x>5){
+            System.out.println("it is greater than 5");
+        }
+        else{
+            System.out.println("it is less than 5");
+        }
+    }
+    
+}
